0ae8c431d117b0226d60d3e969d9c2991ddff75f
Rei.M <kirlo.stnu@gmail.com>
2022-03-30 16:44:36 +0900

    change for some test

---
+++ test/theme.js
@@ -0,0 +1,8 @@
+const theme = _Blocky.getMainWorkspace().getheme();
+
+const component = theme.componentStyles
+
+component.workspaceBackgroundColour = "rgba(0,0,0,1)"
+component.toolboxBackgroundColour = "rgba(255,255,255,0.5)"
+
+_Blocky.getMainWorkspace().setTheme(theme);
