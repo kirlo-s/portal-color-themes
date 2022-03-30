3d9c97b9d9d80b7cf9b55831e22b5196bd466f26
Rei.M <kirlo.stnu@gmail.com>
2022-03-30 17:37:58 +0900

    add components to template

--- template/theme.js
+++ template/theme.js
@@ -2,7 +2,49 @@ const theme = _Blockly.getMainWorkspace().getTheme();
 
 const component = theme.componentStyles
 
-component.workspaceBackgroundColour = "rgba(0,0,0,1)"
-component.toolboxBackgroundColour = "rgba(255,255,255,0.5)"
+component.workspaceBackgroundColour = "rgba(0,0,0,0)"
+component.toolboxBackgroundColour = "rgba(30,39,40,0.8)"
+component.toolboxForegroundColour = "#ffffff"
+component.flyoutBackgroundColour = "#0a0a0a"
+component.flyoutForegroundColour = "#ffffff"
+component.flyoutOpacity = 1
+component.scrollbarColour = "#ffffff"
+component.scrollbarOpacity = 0.5
+component.insertionMarkerColour = "#26ffdf"
+component.insertionMarkerOpacity = .3
+component.markerColour = "#d0d0d0"
+component.cursorColour = "#d0d0d0"
+component.selectedGlowColour = "#26ffdf"
+component.selectedGlowOpacity = 1
+component.replacementGlowOpacity = .5
+component.fieldBorderRectColor = "#a8a8a8"
+component.fieldTextColor = "#000000"
+component.fieldDropdownTextColor = "#ffffff"
+component.fieldDropDownHighlightColor = "#26ffdf"
+component.fieldDropDownTextHighlightColor = "#000000"
+component.fieldEditableTextHoverColor = "#26ffdf"
+component.wsSearchHighlightCurrentColor = "#26ffdf"
+component.wsSearchHighlightGroupColor = "#ffffff"
+component.blockDisablePatternFillColor = "rgba(20,20,20,0.66)"
+component.blockDisablePatternStrokeColor = "#4f4f4f"
+component.blockErrorPatternFillColor = "#141414"
+component.blockErrorPatternStrokeColo = "#aa2b3f"
+component.blockErrorHighlightStrokeColor = "#ffe32b"
+component.blockDeprecatedPatternFillColor = "#383629"
+component.blockDeprecatedPatternStrokeColor = "#a6aa2b"
+component.blockDeprecatedHighlightStrokeColor = "#ffe32b"
+component.ruleBlockRuleTextColor = "#ffffff"
+component.ruleBlockRuleConnectionColor = "#e882ff"
+component.ruleBlockConditionTextColor = "#ffffff"
+component.ruleBlockConditionConnectionColor = "#0a4f78"
+component.ruleBlockActionTextColor = "#ffffff"
+component.ruleBlockActionConnectionColor = "#b7a11c"
+component.subroutineBlockSubroutineTextColor = "#ed7f33"
+component.rulesCategoryColor = "#b45ac7"
+component.subroutinesCategoryColor = "#8a4211"
+component.valuesCategoryColor = "#1d8c58"
+component.actionsCategoryColor = "#b7a11c"
+component.controlsCategoryColor = "#2a90d0"
+
 
 _Blockly.getMainWorkspace().setTheme(theme);
