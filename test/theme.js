dbda392a51cb50af7e750b3a04179f68db6f7c7c
s_t_n\u <kirlo.stnu@gmail.com>
2022-03-31 15:50:16 +0900

    test #11

--- test/theme.js
+++ test/theme.js
@@ -2,18 +2,15 @@ const theme = _Blockly.getMainWorkspace().getTheme();
 
 const component = theme.componentStyles
 
-component.ruleBlockConditionTextColor = "#000000"
-component.ruleBlockConditionConnectionColor = "#00ff00"
-component.ruleBlockActionTextColor = "#000000"
-component.ruleBlockActionConnectionColor = "#0000ff"
+
 
 const block = theme.blockStyles
 
 
 block["mod-block-style"] = { 
-    colourPrimary:"#ffffff",
-    colourSecondary:"#0a0a0a",
-    colourTertiary:"#161d1e",
+    colourPrimary:"#ff0000",
+    colourSecondary:"#00ff00",
+    colourTertiary:"#0000ff",
     hat:"false"
     }
 
